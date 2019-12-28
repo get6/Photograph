b896c723b42b630742d70930051da5b3ade375ff
amapow <amapow25@gmail.com>
2019-12-28 16:01:11 +0900

    add project

---
+++ pretty/src/main/java/com/photograph/pretty/PrettyApplication.java
@@ -0,0 +1,13 @@
+package com.photograph.pretty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrettyApplication.class, args);
+	}
+
+}
---
+++ pretty/src/test/java/com/photograph/pretty/PrettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photograph.pretty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
